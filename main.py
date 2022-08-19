46d68d2d2c048b2e040bebcbdc3e17169651e444
Łukasz Bladziak <lukasz.bladziak200005@gmail.com>
2022-08-19 15:49:27 +0200

    18.08. - wygląd UI modely Haty (z resetowaniem)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from turtle import position
 from PyQt5.QtWidgets import * 
 from PyQt5.QtGui import * 
 import sys
-from PyQt5 import QtCore
+from PyQt5 import QtCore, QtWidgets
 
 #Modules
 import modules.binary
@@ -15,39 +15,75 @@ import modules.mccluskey
 import modules.radio
 import modules.rpn
 
-class Window(QMainWindow):
-    def __init__(self):
-        super().__init__()
-  
-        # set the title
-        self.setWindowTitle("Label")
-  
-        # setting  the geometry of window
-        self.setGeometry(0, 0, 400, 300)
-  
-        # creating a label widget
-        # by default label will display at top left corner
-        self.label_1 = QLabel("Arial font", self)
-        self.label_1.setStyleSheet("border: 1px solid black;")
-        self.label_1.resize(140, 40)
-        self.label_1.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_1.setFont(QFont('Arial', 20))
-        self.label_1.move(170, 100)
-        # moving position
-       
-  
-        # setting font and size
-       
-  
-        self.show()
-  
-  
-  
-# create pyqt5 app
-App = QApplication(sys.argv)
-  
-# create the instance of our Window
-window = Window()
+# class Window(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#
+#         # set the title
+#         self.setWindowTitle("Okno1")
+#
+#         # setting  the geometry of window
+#         self.setGeometry(0, 100, 400, 300)
+#
+#         # creating a label widget
+#         # by default label will display at top left corner
+#         self.label_1 = QLabel("Witaj", self)
+#         self.label_1.setStyleSheet("border: 1px solid black;")
+#         self.label_1.resize(140, 40)
+#         self.label_1.setAlignment(QtCore.Qt.AlignCenter)
+#         self.label_1.setFont(QFont('Arial', 20))
+#         self.label_1.move(170, 100)
+#         # moving position
+#         self.closeButton = QPushButton(self)
+#         self.closeButton.setText("Close")  # text
+#         self.closeButton.setIcon(QIcon("close.png"))  # icon
+#         self.closeButton.setShortcut('Q')  # shortcut key
+#         self.closeButton.clicked.connect(self.close)
+#         self.closeButton.setToolTip("Close the widget")  # Tool tip
+#         self.closeButton.move(100, 100)
+#
+#         # setting font and size
+#
+#
+#         self.show()
 
-# start the app
-sys.exit(App.exec())
+
+# class PushButton(QWidget):
+#     def __init__(self):
+#         super(PushButton,self).__init__()
+#         self.initUI()
+#
+#     def initUI(self):
+#         self.setWindowTitle("PushButton")
+#         self.setGeometry(400,400,300,260)
+#         self.closeButton = QPushButton(self)
+#         self.closeButton.setText("Close")          #text
+#         self.closeButton.setIcon(QIcon("close.png")) #icon
+#         self.closeButton.setShortcut('Q')  #shortcut key
+#         self.closeButton.clicked.connect(self.close)
+#         self.closeButton.setToolTip("Close the widget") #Tool tip
+#         self.closeButton.move(100,100)
+
+  
+if __name__ == '__main__':
+
+    # # create pyqt5 app
+    # App = QApplication(sys.argv)
+    #
+    # # create the instance of our Window
+    # window = Window()
+    # ex = PushButton()
+    #
+    # # start the app
+    # sys.exit(App.exec())
+    app = QApplication(sys.argv)
+    welcome = modules.hata.WelcomeScreen()
+    widget = QtWidgets.QStackedWidget()
+    widget.addWidget(welcome)
+    widget.setFixedHeight(600)
+    widget.setFixedWidth(800)
+    widget.show()
+    try:
+        sys.exit(app.exec_())
+    except:
+        print("Exiting")
--- modules/hata.py
+++ modules/hata.py
@@ -1,30 +1,67 @@
 import math
 
-def exec(f, d, base, mob, mode):
-    if(mode == 1): return get_urban(f, d, base, mob, 1)
-    if(mode == 2): return get_suburban(f, d, base, mob)
-    if(mode == 3): return get_open(f, d, base, mob)
-    raise ValueError("Hata model: invalid mode")
-    return -1
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import QMainWindow, QWidget
 
-def get_a(f, mob, mode):
 
-        if(mode == 1):
-            return (1.1 * math.log10(f) - 0.7)*mob - (1.56*math.log10(f) - 0.8)
 
-        if(mode == 2):
-            if(f < 200):
-                return 8.2*9(math.pow(math.log10(1.54*mob), 2)) - 1.1
+class WelcomeScreen(QMainWindow):
+    def __init__(self):
+        super(WelcomeScreen, self).__init__()
+        loadUi("C:/Users/Lukasz/PycharmProjects/testing/venv/model_haty.ui", self)
+        self.reset_button.clicked.connect(self.go_to_clear_data)
+        # self.f = self.v_input.getDouble()
+        # self.d = self.d_input.getText()
+        # self.base = self.hB_input.getText()
+        # self.mob = self.hM_input.getText()
+        # if self.f != 0:
+        #     print(self.f)
+
+        if self.urban_button.isChecked():
+            self.mode = 1
+        if self.suburban_button.isChecked():
+            self.mode = 2
+        if self.open_button.isChecked():
+            self.mode = 3
+
+
+
+    def go_to_clear_data(self):
+        self.v_input.setText('')
+        self.d_input.setText('')
+        self.hB_input.setText('')
+        self.hM_input.setText('')
+
+
+    def exec(self, f, d, base, mob, mode):
+        if mode == 1: return self.get_urban(f, d, base, mob, 1)
+        if mode == 2: return self.get_suburban(f, d, base, mob)
+        if mode == 3: return self.get_open(f, d, base, mob)
+        raise ValueError("Hata model: invalid mode")
+        return -1
+
+
+    def get_a(self, f, mob, mode):
+        if mode == 1:
+            return (1.1 * math.log10(f) - 0.7) * mob - (1.56 * math.log10(f) - 0.8)
+
+        if mode == 2:
+            if f < 200:
+                return 8.29 * (math.pow(math.log10(1.54 * mob), 2)) - 1.1
             else:
-                return 3.2*(math.pow(math.log10(11.75*mob), 2)) - 4.97
+                return 3.2 * (math.pow(math.log10(11.75 * mob), 2)) - 4.97
 
         return -1
 
-def get_urban(f, d, base, mob, mode = 1):
-    return 69.55 + 26.16 * math.log10(f) - get_a(f, mob, mode) - 13.83 * math.log10(base) + (44.9 - 6.55 * math.log10(base))*math.log10(d)
 
-def get_suburban(f, d, base, mob):
-    return get_urban(f, d, base, mob, 2) - 2*math.pow( math.log10( f/28 ) , 2) - 5.4
+    def get_urban(self, f, d, base, mob, mode=1):
+        return 69.55 + 26.16 * math.log10(f) - self.get_a(f, mob, mode) - 13.83 * math.log10(base) + (
+                44.9 - 6.55 * math.log10(base)) * math.log10(d)
 
-def get_open(f, d, base, mob):
-    return get_urban(f, d, base, mob, 2) - 4.78 * math.pow( math.log10(f) , 2) + 18.33 * math.log10(f) - 40.94
+
+    def get_suburban(self, f, d, base, mob):
+        return self.get_urban(f, d, base, mob, 2) - 2 * math.pow(math.log10(f / 28), 2) - 5.4
+
+
+    def get_open(self, f, d, base, mob):
+        return self.get_urban(f, d, base, mob, 2) - 4.78 * math.pow(math.log10(f), 2) + 18.33 * math.log10(f) - 40.94
