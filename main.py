88e52aff484deb7a158d67903eefb160feed5786
Łukasz Bladziak <lukasz.bladziak200005@gmail.com>
2022-08-30 01:06:42 +0200

    Poprawa UI rejestracji i nazewnictwa funkcji i klas w mainie.

--- main.py
+++ main.py
@@ -10,48 +10,48 @@ class Ekran_poczatkowy(QDialog):
 
     def __init__(self):
         super(Ekran_poczatkowy, self).__init__()
-        loadUi("UI/Wielki_poczatek.ui",self)
-        self.Przycisk_logowania.clicked.connect(self.logowanie)
-        self.Przycisk_nowe_konto.clicked.connect(self.Rejestracja)
+        loadUi("UI/Wielki_poczatek.ui", self)
+        self.przycisk_logowania.clicked.connect(self.logowanie)
+        self.przycisk_nowe_konto.clicked.connect(self.rejestracja)
 
-    def Rejestracja(self):
-        Przycisk_nowe_konto = Ekran_rejestracji()
-        widget.addWidget(Przycisk_nowe_konto)
-        widget.setCurrentIndex(widget.currentIndex()+1)
+    def rejestracja(self):
+        przycisk_nowe_konto = Ekran_rejestracji()
+        widget.addWidget(przycisk_nowe_konto)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
     def logowanie(self):
-        Przycisk_logowania = ekran_logowania()
-        widget.addWidget(Przycisk_logowania)
-        widget.setCurrentIndex(widget.currentIndex()+1)
+        przycisk_logowania = Ekran_logowania()
+        widget.addWidget(przycisk_logowania)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
 
-class ekran_logowania(QDialog):
+class Ekran_logowania(QDialog):
 
     def __init__(self):
-        super(ekran_logowania,self).__init__()
-        loadUi("UI/Logowanie.ui",self)
-        self.pole_haslo.setEchoMode(QtWidgets.QLineEdit.Password) #kropeczki wpisujac haslo
+        super(Ekran_logowania, self).__init__()
+        loadUi("UI/Logowanie.ui", self)
+        self.pole_haslo.setEchoMode(QtWidgets.QLineEdit.Password)  # kropeczki wpisujac haslo
         self.login.clicked.connect(self.funkcja_logowania)
 
     def funkcja_logowania(self):
         nazwa_uzytkownika = self.pole_nazwa_uzytkownika.text()
         haslo = self.pole_haslo.text()
 
-        if(len(nazwa_uzytkownika)== 0 or len(haslo)==0):
-            self.blad.setText("Nieprawidłowa nazwa użytkownika, bądź hasło")
+        if (len(nazwa_uzytkownika) == 0 or len(haslo) == 0):
+            self.blad.setText("Nieprawidłowa nazwa użytkownika lub hasło!")
         else:
             polaczenie = sqlite3.connect("baza_danych_uzytkownikow.db")
             cur = polaczenie.cursor()
-            wiersz = 'SELECT password FROM login_info WHERE username =\''+nazwa_uzytkownika+"\'"
+            wiersz = 'SELECT password FROM login_info WHERE username =\'' + nazwa_uzytkownika + "\'"
             cur.execute(wiersz)
-            rezultat= cur.fetchone()[0]
+            rezultat = cur.fetchone()[0]
             if rezultat == haslo:
                 print("Logowanie powiodło się!")
                 profil = Menu()
                 widget.addWidget(profil)
                 widget.setCurrentIndex(widget.currentIndex() + 1)
             else:
-                self.blad.setText("Nieprawodłowa nazwa użytkownika bądz hasło")
+                self.blad.setText("Nieprawidłowa nazwa użytkownika lub hasło!")
 
 
 class Ekran_rejestracji(QDialog):
@@ -65,12 +65,12 @@ class Ekran_rejestracji(QDialog):
 
     def funkcja_rejestracji(self):
         nazwa_uzytkownika_rejestracja = self.pole_nazwa_uzytkownika2.text()
-        haslo_rejestracja= self.pole_haslo2.text()
-        haslo2_rejestacja= self.pole_haslo2_podtwierdzenie.text()
+        haslo_rejestracja = self.pole_haslo2.text()
+        haslo2_rejestacja = self.pole_haslo2_podtwierdzenie.text()
 
-        if(len(nazwa_uzytkownika_rejestracja)==0 or len(haslo_rejestracja)==0 or len(haslo2_rejestacja)==0):
-            self.blad2.setText("Prosze wypełnij puste pola.")
-        elif haslo_rejestracja!=haslo2_rejestacja:
+        if (len(nazwa_uzytkownika_rejestracja) == 0 or len(haslo_rejestracja) == 0 or len(haslo2_rejestacja) == 0):
+            self.blad2.setText("Proszę wypełnij puste pola.")
+        elif haslo_rejestracja != haslo2_rejestacja:
             self.blad2.setText("Hasła są różne.")
         else:
             polaczenie2 = sqlite3.connect("baza_danych_uzytkownikow.db")
@@ -90,23 +90,23 @@ class Menu(QDialog):
     def __init__(self):
         super(Menu, self).__init__()
         loadUi("UI/Menu.ui", self)
-        self.modelHaty_przycisk.clicked.connect(self.Model_Haty)                                   # menu główne, przycisk 1
-        self.Operacja_2_przycisk.clicked.connect(self.Operacja2)                                    # menu główne, przycisk 2ss222
-        self.Operacja_3_przycisk.clicked.connect(self.Operacja3)
+        self.modelHaty_przycisk.clicked.connect(self.model_Haty)  # menu główne, przycisk 1
+        self.operacja_2_przycisk.clicked.connect(self.operacja2)  # menu główne, przycisk 2ss222
+        self.operacja_3_przycisk.clicked.connect(self.operacja3)
 
-    def Model_Haty(self):
-        Operacja_1_przycisk = Model_Haty()
-        widget.addWidget(Operacja_1_przycisk)
+    def model_Haty(self):
+        modelHaty_przycisk = Model_Haty()
+        widget.addWidget(modelHaty_przycisk)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
-    def Operacja2(self):
-        Operacja_2_przycisk = Operacja2()
-        widget.addWidget(Operacja_2_przycisk)
+    def operacja2(self):
+        operacja_2_przycisk = Operacja2()
+        widget.addWidget(operacja_2_przycisk)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
-    def Operacja3(self):
-        Operacja_3_przycisk = Operacja3()
-        widget.addWidget(Operacja_3_przycisk)
+    def operacja3(self):
+        operacja_3_przycisk = Operacja3()
+        widget.addWidget(operacja_3_przycisk)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
 
