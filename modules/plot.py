3e7adbdc3aa145663cc558f59668ebd8f31361ec
gjaskula99 <81091594+gjaskula99@users.noreply.github.com>
2022-08-31 16:48:09 +0200

    Ploter implemented

--- modules/plot.py
+++ modules/plot.py
@@ -1,34 +1,63 @@
-#To jest do ogarnięcia. Plotuje matplotlib do Qt
-
 import sys
-import matplotlib
-matplotlib.use('Qt5Agg')
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout
 
-from PyQt5 import QtCore, QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
 
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
-from matplotlib.figure import Figure
+import random
 
+class Window(QDialog):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
 
-class MplCanvas(FigureCanvasQTAgg):
+        # a figure instance to plot on
+        self.figure = plt.figure()
 
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        super(MplCanvas, self).__init__(fig)
+        # this is the Canvas Widget that displays the `figure`
+        # it takes the `figure` instance as a parameter to __init__
+        self.canvas = FigureCanvas(self.figure)
 
+        # this is the Navigation widget
+        # it takes the Canvas widget and a parent
+        self.toolbar = NavigationToolbar(self.canvas, self)
 
-class MainWindow(QtWidgets.QMainWindow):
+        # Just some button connected to `plot` method
+        self.button = QPushButton('Plot random')
+        self.button.clicked.connect(self.plot)
 
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-        sc = MplCanvas(self, width=5, height=4, dpi=100)
-        sc.axes.plot([0,1,2,3,4], [10,1,20,3,40]) #DANE WŁAŚCIWE
-        self.setCentralWidget(sc)
+        # set the layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.toolbar)
+        layout.addWidget(self.canvas)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
 
-        self.show()
+    def plot(self, data):
+        # random data
+        if(data == False): data = [random.random() for i in range(10)]
+        print(data)
 
+        # instead of ax.hold(False)
+        self.figure.clear()
 
-app = QtWidgets.QApplication(sys.argv)
-w = MainWindow()
-app.exec_()
+        # create an axis
+        ax = self.figure.add_subplot(111)
+
+        # discards the old graph
+        # ax.hold(False) # deprecated, see above
+
+        # plot data
+        ax.plot(data, '*-')
+
+        # refresh canvas
+        self.canvas.draw()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    main = Window()
+    main.show()
+    main.plot([-5,-4,-3,-2,-1,0,1,2,3,4,5])
+
+    sys.exit(app.exec_())
